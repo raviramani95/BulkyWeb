1a30d837d38f27342f12eaac12e528f519454f39
RamaniRavi <ramaniravi196@gmail.com>
2023-06-10 12:59:17 +0530

    6.90 View Models

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -42,8 +43,10 @@ namespace Bulky.Models
 
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
-        public Category Category { get; set; }
+        [ValidateNever]
 
+        public Category Category { get; set; }
+        [ValidateNever]
         public string ImageUrl { get; set; }
     }
 }
